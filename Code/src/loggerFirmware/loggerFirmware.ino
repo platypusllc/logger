017b3c447897525099218f6048a892509b7f8bae
ckt <christomaszewski@gmail.com>
2014-07-05 14:21:32 -0400

    Finised first pass at the logger code. Code reads DO probe and logs timestamped readings to the sdcard. RTC clock configuration still needs work.

--- Code/src/loggerFirmware/loggerFirmware.ino
+++ Code/src/loggerFirmware/loggerFirmware.ino
@@ -1,9 +1,11 @@
+#include <Wire.h>
 #include <RTClib.h>
 #include <SD.h>
-#include <Wire.h>
+
 
 //Logging configuration
 #define LOG_INTERVAL 1000 //Milliseconds between log entries
+#define SYNC_INTERVAL 1000 //Milliseconds between SD card flushes
 
 //LED pin definitions
 #define greenLEDpin 2
@@ -17,10 +19,17 @@ RTC_DS1307 RTC;
 const int chipSelect = 10;
 
 //RTC Config Filename
-const char[] rtcConfigFile = "RTC.cfg";
+char rtcConfigFile[] = "RTC.cfg";
 
 //Instantiate File object for logfile
 File logfile;
+
+//Set last sync time
+uint32_t syncTime = 0;
+
+//Instantiate variables used in reading DO probe
+String sensorString = "";
+bool stringComplete = false;
 
 //SD Error Function - Lights up Red LED and halts execution
 void sdError(char *str){
@@ -42,8 +51,11 @@ void waitForSD(void){
 
 //RTC Configuration function - configures RTC according to info found in RTC.cfg
 bool rtcConfig(){
+  if (!SD.exists(rtcConfigFile)){
+    return true;
+  }
   
-   return true; 
+  return true; 
 }
 void setup(void){
   //Startup hardware serial
@@ -61,5 +73,97 @@ void setup(void){
   }
   
   //Create a new file to log to
-  char filename[] = "LOG
+  char filename[] = "LOG00.csv";
+  for (uint8_t i = 0; i < 100; i++){
+    filename[3] = i/10 + '0';
+    filename[4] = i%10 + '0';
+    if (!SD.exists(filename)){
+      logfile = SD.open(filename, FILE_WRITE);
+      break;
+    }
+  }
   
+  if (!logfile){
+    sdError("ERROR: Could not create log file");
+  }
+  
+  Wire.begin();
+  if (!RTC.begin()){
+    logfile.println("WARNING: RTC failed");
+  }
+  
+  logfile.println("millis,time,DO");
+  /*if (logfile.writeError || !logfile.sync()){
+    sdError("ERROR: Could not write header to card");
+  }*/
+  
+  pinMode(greenLEDpin, OUTPUT);
+  pinMode(yellowLEDpin, OUTPUT);
+  pinMode(redLEDpin, OUTPUT);
+  
+  //Start Hardware serial for communication to DO Probe
+  Serial.begin(38400);
+  Serial.print("E\r");
+  
+  //Set aside some bytes for receiving data from sensor
+  sensorString.reserve(30);
+}
+
+void loop(void){
+  DateTime now;
+  
+  delay((LOG_INTERVAL - 1) - (millis() % LOG_INTERVAL));
+  
+  digitalWrite(greenLEDpin, HIGH);
+  
+  uint32_t m = millis();
+  logfile.print(m);
+  logfile.print(",");
+  
+  now = RTC.now();
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print(",");
+  
+  
+  //Read DO probe
+  digitalWrite(yellowLEDpin, HIGH);
+  Serial.print("R\r");
+  delay(650);
+  
+  while (Serial.available()||!stringComplete){
+    char incomingChar = (char)Serial.read();
+    sensorString += incomingChar;
+    if (incomingChar == '\r'){
+      stringComplete = true;
+    }
+  }
+  
+  digitalWrite(yellowLEDpin, LOW);
+  
+  //Write sensor data to card and clear buffer and flag
+  logfile.print(sensorString);
+  sensorString = "";
+  stringComplete = false;
+  
+  //Skip flush if sync interval has not been reached
+  if ((millis() - syncTime) < SYNC_INTERVAL){
+    return;
+  }
+  syncTime = millis();
+  
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  logfile.flush();
+  digitalWrite(redLEDpin, LOW);
+  digitalWrite(greenLEDpin, LOW);
+}   
